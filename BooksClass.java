9245a906e82090e0df2d02bc0d6c68ae635fee7e
IrihoDocile <78589531+IrihoDocile@users.noreply.github.com>
2024-02-18 19:32:13 +0200

    Assignment

---
+++ BooksClass.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.util.Scanner;
+/**
+ *
+ * @author user
+ */
+public class BooksClass {
+    static String booknm1;
+    static String booknm2;
+    static String booknm3;
+    static int nbrBook;
+    public static void main(String[] args) {
+        Scanner getUserInput=new Scanner(System.in);
+        System.out.println("Enter the number of the books");
+        nbrBook=getUserInput.nextInt();
+        if(nbrBook<=3){
+            for(int i=1;i<=nbrBook;i++){
+                System.out.println("Enter"+" "+nbrBook+" "+"Books name");
+                if(nbrBook==1){
+                    booknm1=getUserInput.next();
+                    System.out.println("you are allowed"+booknm1);
+                    
+                }
+              
