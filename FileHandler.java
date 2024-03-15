55ca9afe10daad9c15ca75fa146931be2a7f2d7a
AlJudicael <159712766+AlJudicael@users.noreply.github.com>
2024-03-15 15:34:26 -0700

    Add files via upload

---
+++ FileHandler.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Product;
+
+/**
+ *
+ * @author uyser
+ */
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+//import org.apache.poi.ss.usermodel.*;
+
+public class FileHandler {
+    // Method to read product information from an Excel file and update the inventory
+    public void readFromExcel(File file, Inventory inventory) throws IOException {
+        // Read from the Excel file and update the inventory
+    }
+
+    // Method to write the current inventory information to an Excel file for record-keeping
+    public void writeToExcel(File file, Inventory inventory) throws IOException {
+        // Write the current inventory information to the Excel file
+    }
+}
